14646a0afc3da3bc2a41e1479da1136d643dd2c8
Mikaela Grace <mikaelagrace@users.noreply.github.com>
2022-11-17 15:26:44 -0500

    Update task1.py
    No need to merge unless you want to -- just a way for me to comment/edit inline

--- Task1/task1.py
+++ Task1/task1.py
@@ -39,7 +39,15 @@ class Task1():
         
     """
     
-    def __init__(self, dataset_path = "../Datasets/task1_dataset.txt"):
+    def __init__(self, dataset_path = "../Datasets/task1_dataset.txt"): 
+        # Your homework looks great already! You do not need to change this in this homework, but I wanted to put here more as just a learning/advancement point: 
+        # recently, there has been a shift away from defining types in comments, and rather people just use the typing library to annotate type. 
+        # that is, you would do this:
+        #
+        # from typing import Optional
+        # def __init__(self, dataset_path: Optional[str]= "../Datasets/task1_dataset.txt")
+        #
+        # https://mypy.readthedocs.io/en/stable/cheat_sheet_py3.html for more info.
         """
         Construct attributes for the class.
 
@@ -70,17 +78,15 @@ class Task1():
         
         """
         # Open file
-        file = open(self.datasetPath, 'r')
-        
-        # Read lines and close file
-        lines = file.readlines()
-        file.close()
-        
-        if verbose: print("Lines extracted from: '", self.datasetPath, "'")
-        
-        return lines
+        # Use the With construction to automatically close! 
+        # https://www.programiz.com/python-programming/file-operation
+        with open((self.datasetPath, 'r') as f: 
+            lines = file.readlines()
+            if verbose:
+                  print("Lines extracted from: '", self.datasetPath, "'")
+            return lines
 
-    def __infer(self, lines,  verbose = False):
+    def __infer(self, lines,  verbose = False): # Nit: should be one underline instead of two! https://towardsdatascience.com/whats-the-meaning-of-single-and-double-underscores-in-python-3d27d57d6bd1
         """
         Make inferences for each line.
 
@@ -106,7 +112,7 @@ class Task1():
             inference = self.sentiment_pipeline(data)
             inferences.append(inference[0]["label"])
         
-        if verbose: print("Inferences made:", str(len(inferences)))
+        if verbose: print("Num nferences made: ", str(len(inferences)))
         
         return inferences
     
@@ -140,7 +146,15 @@ class Task1():
             
         return inferences
     
-    def test(self, verbose = True):
+    def test(self, verbose = True): 
+        # Note: you dont need to refactor for this homework, since I wasn't specific enough in the writeup. 
+        # but for actual industry tasks, you should not add the tests 
+        # to the class object. Rather, put all the tests in files in the test folder, and import the class you are trying to test and then 
+        # write the tests there using the class.
+        # This separates the actual class logic from testing logic, which sometimes will require mocks etc that you dont want to import with the business logic of the class. 
+        # it also allows you to run all the tests at once with pytest, which is used in most setups to prevent merging unless tests pass
+                  
+                  
         """
         Test implementation with a controlled dataset.
 
